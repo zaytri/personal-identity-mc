7c4b26746586df04be535aa4daf7d442cdee9264
Zaytri <9072119+zaytri@users.noreply.github.com>
2024-03-01 21:08:16 -0800

    fabric init

---
+++ src/client/kotlin/com/zaytri/PersonalIdentityClient.kt
@@ -0,0 +1,9 @@
+package com.zaytri
+
+import net.fabricmc.api.ClientModInitializer
+
+object PersonalIdentityClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
